71e16b35314467aa18d9f431cf1aadeb6c38efe5
MaulanaSeto <maulana.seto@ui.ac.id>
2024-02-07 16:54:25 +0700

    Init

---
+++ src/main/java/id.ac.ui.cs.advprog.eshop/model/ProductTest.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org..junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+    Product product;
+
+    @BeforeEach
+    void setUp() {
+        this.product = new Product();
+        
+    }
+}
