2af3bebd12bf63738ab8ef3a7d9f943d8a365388
MaulanaSeto <maulana.seto@ui.ac.id>
2024-02-24 11:37:59 +0700

    Edit Product

--- src/main/java/id.ac.ui.cs.advprog.eshop/model/Product.java
+++ src/main/java/id.ac.ui.cs.advprog.eshop/model/Product.java
@@ -3,8 +3,17 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Getter @Setter
 public class Product {
-    private String productId, productName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String productName;
     private int productQuantity;
 }
