4a4275819d0e8734cdb86c824a73888864ccd186
MaulanaSeto <maulana.seto@ui.ac.id>
2025-02-20 16:36:56 +0700

    HomePage Feature

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/MainControllerTest.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+class MainControllerTest {
+    @Test
+    void testHomePage() throws Exception {
+        MainController controller = new MainController();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(get("/")).andExpect(view().name("HomePage"));
+    }
+}
