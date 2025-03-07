c72c5989104754d7d1fa93bbd44f2a1e6ff3ecef
MaulanaSeto <maulana.seto@ui.ac.id>
2025-03-07 20:07:44 +0700

    [GREEN] Implement PaymentCoD model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentCoD.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentCoD.java
@@ -1,9 +1,30 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+
 import java.util.Map;
 
 public class PaymentCoD extends Payment {
     public PaymentCoD(String id, Order order, Map<String, String> paymentData) {
         super(id, "CoD", order, paymentData);
+
+        if (paymentData.containsKey("address") && isAddressValid(paymentData.get("address"))
+                && paymentData.containsKey("deliveryFee") && isDeliveryFeeValid(paymentData.get("deliveryFee"))) {
+            super.setStatus(PaymentStatus.SUCCESS.getValue());
+        } else {
+            super.setStatus(PaymentStatus.REJECTED.getValue());
+        }
+    }
+
+    private boolean isAddressValid(String addr) {
+        return !addr.isBlank();
+    }
+
+    private boolean isDeliveryFeeValid(String fee) {
+        try {
+            return Integer.parseInt(fee) >= 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
